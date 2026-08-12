4b33bac6fcc50b34591eec49975ef168994939c3
lealfs <felipesantosleal.fl@gmail.com>
2026-08-11 21:26:06 -0300

    Add files via upload

---
+++ untitled/src/main/java/org/example/Aritmeticos.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Aritmeticos {
+    public static void main(String[] args) {
+        //Declaração de variaveis
+        int a = 10;
+        int b = 2;
+
+        //Operadores aritmeticos
+        int multiplicar = a * b;
+        int dividir = a / b;
+        int somar = a + b;
+        int subtrair = a - b;
+
+        //Exibindo resultados
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+    }
+}
