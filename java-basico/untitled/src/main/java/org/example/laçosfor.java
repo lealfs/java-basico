9299d3fd998e572a8dc23d76dae8fc57054603fc
lealfs <felipesantosleal.fl@gmail.com>
2026-08-12 21:25:50 -0300

    Add files via upload

---
+++ java-basico/untitled/src/main/java/org/example/laçosfor.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class laçosfor {
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++){
+            System.out.println("Valor de i: " +i);
+        }
+    }
+}
